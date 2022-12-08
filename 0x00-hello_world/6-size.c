7e832ac6b6271622daf6e3f4d6b5312d70451f8b
Kunle Ademikanra <kunleademikanra@gmail.com>
2022-12-08 07:59:12 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 
 int main(void)
-{ 
+{
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
@@ -14,7 +14,8 @@ int main(void)
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
-/* Note!
+/*
+ * Note!
  * You should get something like this as an output
  * which are the sizes of the built in data types in C
  Size of a char: 1 byte(s) which is 8 bits, example is letter a
